6462b1fecce6118cba089c750a7ccb406e62ec1f
Chris Gomez <akagomez@gmail.com>
2018-03-23 12:58:05 -0400

    Personalize landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 style={{
+      fontStyle: 'italic'
+    }}>Why, Hello!!!</h1>
+    <p>
+      I'm <strong>Chris Gomez</strong>, a{' '}
+      <a href="https://en.wikipedia.org/wiki/Servant_leadership">servant leader</a>,{' '}
+      <a href="https://en.wikipedia.org/wiki/Software_developer">software developer</a>,{' '}
+      and obsessive <a href="https://en.wikipedia.org/wiki/Critical_thinking">critical thinker</a>{' '}
+      that has evolved from more than 10 years of experience building products
+      in the consulting, media, marketing, banking, open source, and SaaS industries.{' '}
+      <strong>Welcome to my personal website!</strong>
+    </p>
+    <p>
+      <code>// TODO: Document professional history in detail</code>
+    </p>
   </div>
 );
 
