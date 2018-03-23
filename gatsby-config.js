f319c7403de78944f68aed5249e36495dde0e03c
Chris Gomez <akagomez@gmail.com>
2018-03-23 16:45:27 -0400

    Add Google Analytics/Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Chris Gomez | Leader, Maker, and Innovator',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-116277831-1',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-W6V7C5K",
+        includeInDevelopment: false,
+      },
+    },
+  ],
 };
