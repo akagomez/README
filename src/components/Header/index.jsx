a08f7656ed281da1a65f071ba953ed1068c5e724
Chris Gomez <akagomez@gmail.com>
2018-03-23 11:31:33 -0400

    Personalize the Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,29 +4,27 @@ import Link from 'gatsby-link';
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      margin: '0 auto',
+      maxWidth: 960,
+      padding: '1.45rem 1.0875rem',
     }}
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
+    <h1 style={{ margin: 0 }}>
+      <a
+        href="https://github.com/akagomez/"
+        style={{
+          textDecoration: 'none',
+        }}>
+        akagomez
+      </a> / <Link
+        to="/"
+        style={{
+          textDecoration: 'none',
+        }}
+      >
+        README
+      </Link>
+    </h1>
   </div>
 );
 
